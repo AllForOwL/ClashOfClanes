dda8ca0d6fe4807fddefc8fa725586c1ea32275f
AllForOwl <sahavrysyuk@gmail.com>
2016-10-27 15:10:18 +0400

    Add camera

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -18,78 +18,69 @@ Scene* HelloWorld::createScene()
     return scene;
 }
 
-// on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Layer::init() )
+    if (!Layer::init())
     {
         return false;
     }
     
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    m_visibleSize	= Director::getInstance()->getVisibleSize();
+    Vec2 origin		= Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
-    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+	m_rectBottomLeft.setRect	(0, 0, m_visibleSize.width / 2, m_visibleSize.height / 2);
+	m_rectBottomRight.setRect	(m_visibleSize.width / 2, 0, m_visibleSize.width / 2, m_visibleSize.height / 2);
+	m_rectTopLeft.setRect		(0, m_visibleSize.height / 2, m_visibleSize.width / 2, m_visibleSize.height / 2);
+	m_rectTopRight.setRect		(m_visibleSize.width / 2, m_visibleSize.height / 2, m_visibleSize.width / 2, m_visibleSize.height / 2);
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+	m_map = TMXTiledMap::create("E:/programming/cocos/project/ClashOfClans/Resources/res/backgrounds/Map.tmx");
+	m_map->setPosition(-m_visibleSize.width * 3,
+		m_visibleSize.height / 2 + origin.y);
+	
+	this->addChild(m_map);
 
-    /////////////////////////////
-    // 3. add your codes below...
+	auto _winSize	= Director::getInstance()->getWinSize();
+	m_camera = Camera::createOrthographic(m_visibleSize.width, m_visibleSize.height, -300, 10000);
+	m_camera->setPosition3D(Vec3(0, 0, 0));
+	this->addChild(m_camera);
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
+	auto _touchListener = EventListenerTouchOneByOne::create();
+	_touchListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(_touchListener, this);
 
-    // add the label as a child to this layer
-    this->addChild(label, 1);
+	//schedule(schedule_selector(HelloWorld::update), 0.1);
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
     return true;
 }
 
-
-void HelloWorld::menuCloseCallback(Ref* pSender)
+/*virtual*/ bool HelloWorld::onTouchBegan(cocos2d::Touch* i_touch, cocos2d::Event* i_event)
 {
-    //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
+	Touch* _touch = i_touch;
+	Vec2 _locationTouch = _touch->getLocation();
+	Vec3 _positionCamera = m_camera->getPosition3D();
+	if (m_rectBottomLeft.containsPoint(_locationTouch))
+	{
+		m_camera->setPosition3D(Vec3(_positionCamera.x - 2, _positionCamera.y, _positionCamera.z));
+	}
+	else if (m_rectBottomRight.containsPoint(_touch->getLocation()))
+	{
+		m_camera->setPosition3D(Vec3(_positionCamera.x + 2, _positionCamera.y, _positionCamera.z));
+	}
+	else if (m_rectTopLeft.containsPoint(_locationTouch))
+	{
+		m_camera->setPosition3D(Vec3(_positionCamera.x, _positionCamera.y + 2, _positionCamera.z));
+	}
+	else
+	{
+		m_camera->setPosition3D(Vec3(_positionCamera.x, _positionCamera.y - 2, _positionCamera.z));
+	}
 
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
-    
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-    
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-    
-    
+	return true;
+}
+
+/*virtual*/ void HelloWorld::update(float dt)
+{
+	this->setPositionX(this->getPositionX() - 50);
+	this->setPositionY(this->getPositionY() - 50);
 }
--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -9,12 +9,24 @@ public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
-    
-    // implement the "static create()" method manually
+   
     CREATE_FUNC(HelloWorld);
+
+	virtual void update(float dt);
+
+	virtual bool onTouchBegan(cocos2d::Touch* i_touch, cocos2d::Event* i_event);
+
+private:
+	cocos2d::Size			m_visibleSize;
+	cocos2d::TMXTiledMap*	m_map;
+	cocos2d::Camera*		m_camera;
+	
+	cocos2d::Rect			m_rectBottomLeft;
+	cocos2d::Rect			m_rectBottomRight;
+	cocos2d::Rect			m_rectTopRight;
+	cocos2d::Rect			m_rectTopLeft;
+
+
 };
 
 #endif // __HELLOWORLD_SCENE_H__
